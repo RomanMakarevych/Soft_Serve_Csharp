affecc69e3c51374bb9c128e0f4652fd161d1eaa
Fun_2 <Fun_2@DESKTOP-S6APOTJ>
2023-04-21 11:53:57 +0200

    Add project files.

---
+++ Soft_Serve_Csharp/Program.cs
@@ -0,0 +1,50 @@
+﻿
+// Home Work 1 
+
+
+
+//Task 1 
+
+//Console.WriteLine("Enter the length of Side: ");
+//int side = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Lenght of Side ist {side} ");
+//int area = (int)Math.Pow(side,2);
+//Console.WriteLine($"\n The squere area is: {area}");
+//int perimeter = 4*side;
+//Console.WriteLine($"\n The squere perimeter is: {perimeter}");
+//Console.WriteLine(new String('-',30));
+
+// Task 2 
+
+//string name;
+//int age;
+//Console.WriteLine("What is your name?");
+//name = Console.ReadLine();
+//Console.WriteLine($"My name is - {name}");
+//Console.WriteLine($"How old are you,{name}?");
+//age=int.Parse(Console.ReadLine());
+//Console.WriteLine($"I'm {age}");
+//Console.WriteLine(new String('-',30));
+
+//Task 3 
+
+//double radius, lenght, area, volume;
+//Console.WriteLine("Enter the circle Radius: ");
+//radius = double.Parse(Console.ReadLine());
+
+//lenght = (double)(2 * Math.PI * radius);
+//string formatted = lenght.ToString("F3");// вивiд 3 знакiв пiсля коми
+//Console.WriteLine($"\nThe Lenght is {formatted}");
+
+//area = (double)(Math.PI * Math.Pow(radius, 2));
+//string formatted2 = area.ToString("F3");
+//Console.WriteLine($"\nThe Area is {formatted2}");
+
+//volume = (double)(4 / 3 * Math.PI * Math.Pow(radius, 3));
+//string formatted3 = volume.ToString("F3");
+//Console.WriteLine($"\n The Volume is {formatted3}");
+//Console.WriteLine(new String('-', 40));
+
+//Console.ReadKey();
+
+
