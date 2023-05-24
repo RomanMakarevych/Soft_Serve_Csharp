ab727830288ed1b595be59fa6a2e68cf91a41353
RomanMakarevych <0997483958@ukr.net>
2023-05-24 20:27:48 +0200

    hw11

---
+++ Home_Work_11/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace Home_Work_11
+{
+    public delegate void MyDel(int m);
+
+    class Student
+    {
+        private string? name;
+        private List<int> marks;
+        public event MyDel MarkChange;
+
+      
+        public Student(string name, List<int> marks)
+        {
+            this.name = name;
+            this.marks = marks;
+            
+        }
+        public void AddMark(int mark)
+        {
+            if(mark < 0 || mark > 12)
+                throw new ArgumentOutOfRangeException( "mark","The mark must be between 0 and 12.");
+            marks.Add(mark);
+            MarkChange?.Invoke(mark);
+        }
+
+        public void Print()
+        {
+            Console.Write($"{name}: ");
+            foreach (var item in marks)
+            {
+                Console.Write(item + " ") ;
+            }
+        }
+
+    }
+
+    class Parent
+    {
+        public void OnMarkChange(int mark)
+        {
+            Console.WriteLine($"New mark: {mark}");
+        }
+    }
+
+    class Accountancy
+    {
+        public void PayingFellowship(int mark)
+        {
+            if (mark >= 10)
+            {
+                Console.WriteLine("The student is eligible for a scholarship.");
+            }
+            else
+            {
+                Console.WriteLine("The student is not eligible for a scholarship.");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new("Roman", new List<int> { });
+            Parent parent = new();
+            Accountancy accountancy = new();
+            student.MarkChange += parent.OnMarkChange;
+            student.MarkChange += accountancy.PayingFellowship;
+
+
+            try
+            {
+                student.AddMark(10);
+                student.AddMark(9);
+                student.AddMark(12);
+                student.Print();
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error:" + ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ Task_8/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_8
+{
+    class Person : IComparable<Person>
+    {
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public  string? Name { get { return name; } }
+
+        public int CompareTo(Person other)
+        {
+            return this.name.CompareTo(other.name);
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Person Name: {name}");
+        }
+    }
+
+    class Staff : Person
+    {
+        private int salary;
+        public Staff(string name, int salary) : base(name)
+        {
+            this.salary = salary;
+        }
+
+   
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($" has salary: {salary}");
+        }
+    }
+
+    class Teacher : Staff
+    {
+        private string subject;
+        public Teacher(string name, int salary, string subject) : base(name, salary)
+        {
+            this.subject = subject;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Subject: {subject}");
+        }
+    }
+
+    class Developer : Staff
+    {
+        private int level;
+        public Developer(string name, int salary, int level) : base(name, salary)
+        {
+            this.level = level;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Level: {level}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            persons.Add(new Staff("Petro", 1200));
+            persons.Add(new Teacher("Anton", 1000, "Englisch"));
+            persons.Add(new Developer("Roman", 5000, 1));
+
+            foreach (Person person in persons)
+            {
+                person.Print();
+            }
+
+            try
+            {
+                Console.WriteLine("Enter a name:");
+                string name = Console.ReadLine();
+                bool nameFound = false;
+
+                foreach (Person person in persons)
+                {
+                    if (person.Name==name)
+                    {
+                        Console.WriteLine($"The name {name} is in the list of persons");
+                        nameFound = true;
+                        break;
+                    }
+                }
+
+                if (!nameFound)
+                {
+                    Console.WriteLine("The name was not found");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("It's not in the correct format");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(new string('-',50));
+
+            persons.Sort();
+            foreach (Person person in persons)
+            {
+                person.Print();
+            }
+
+        }
+    }
+}
